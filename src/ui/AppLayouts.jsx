3402740a75abd29966927c38d2db915d0024d1d4
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-09 15:40:01 +0100

    Displaying a Loading Indicator

--- src/ui/AppLayouts.jsx
+++ src/ui/AppLayouts.jsx
@@ -1,18 +1,22 @@
-import Header from "./Header" 
-import CartOverView from '../features/cart/CartOverview'
-import { Outlet } from "react-router-dom"
+import Header from "./Header";
+import CartOverView from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayouts() {
+  const navigation = useNavigation();
+  const isLoading=navigation.state === 'loading';
   return (
-    <div>
-      <Header/>
+    <div className="layout">
+      {isLoading && <Loader/>}
+      <Header />
       <main>
         <h1>Contenet</h1>
-        <Outlet/>
+        <Outlet />
       </main>
-     <CartOverView/>
+      <CartOverView />
     </div>
-  )
+  );
 }
 
-export default AppLayouts
+export default AppLayouts;
