b97132b56c320404d31f10c432fe6eef1ffae115
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-24 13:26:20 +0100

    Fetching Data Without Navigation: useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
+import { useFetcher, useLoaderData } from 'react-router-dom';
 
-import { useLoaderData } from 'react-router-dom';
 import OrderItem from './OrderItem';
 import { getOrder } from '../../services/apiRestaurant';
 import {
@@ -8,10 +8,19 @@ import {
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+  const fetcher = useFetcher();
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher]
+  );
+  console.log(fetcher.data);
 
   const {
     id,
@@ -51,9 +60,16 @@ function Order() {
           (Estimated delivery: {formatDate(estimatedDelivery)})
         </p>
       </div>
-      <ul className=' divide-y divide-stone-200 border-y'>
+      <ul className=" divide-y divide-stone-200 border-y">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
       <div className="space-y-2 bg-stone-200 px-6 py-5">
